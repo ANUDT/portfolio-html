ca8866a194f0dcda38322bb61d5c296370a92eb3
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-05 20:51:45 +0100

    braces error in header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,14 +10,14 @@ function Header() {
       <header>
         <h1>MY PORTFOLIO</h1>
         <Navigation />
-
+        </header>
         <div className="h-content">
           <div className="h-section about">
             <h1 className="logo-text">MyPortfolio</h1>
             <p>
             MY PORTFOLIO SHOWCASES THE PROJECTS AND ACHIEVEMENTS OF A DEDICATED WEB DEVELOPER AND DESIGNER
             </p>
-            
+           
             <div className="socials">
               <a href="#"><i className="fab fa-github"></i></a>
               <a href="#"><i className="fab fa-linkedin"></i></a>
@@ -44,11 +44,11 @@ function Header() {
         </div>
         <div className="h-bottom">&copy; adt.com | Designed by Me</div>
         <div className="headText container">
-          <img src={headText1} className="headText-1" alt="HeadText 1" />
+          <img src={headText1} className="headText-1 style=width:100%" alt="HeadText 1" />
           <a href="https://github.com">HeadText 1</a>
-          <img src={headText2} className="headText-2" alt="HeadText 2" />
+          <img src={headText2} className="headText-2 style=width:100%" alt="HeadText 2" />
           <a href="https://github.com">HeadText 2</a>
-          <img src={headText3} className="headText-3" alt="HeadText 3" />
+          <img src={headText3} className="headText-3 style=width:100%" alt="HeadText 3" />
           <a href="https://github.com">HeadText 3</a>
         </div>
         <div className="h-section links">
@@ -63,5 +63,7 @@ function Header() {
       <header>
   {/* Your header content */}
 </header>
+</div>
+)}
 
-export default Footer;
+export default Header;
