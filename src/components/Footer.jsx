054852802ad62b8dae014c383b2bdd4e6d83d95b
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-05 19:21:20 +0100

    images and css footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import "../App.css";
+import footText1 from "../../image/Designer.jpeg"
+import footText2 from "../../image/Designer.jpeg"
+import footText3 from "../../image/Designer.jpeg"
 
 function Footer() {
   return (
     <header>
       <h1>Footer</h1>
+      <div className="footText container">
+         <img src={footText1} className="footText 1"/>
+         <a href= "Github.com">footText 1</a>
+          <img src={footText2} className="footText 2"/>
+          <a href= "Github.com">FootText 2</a>
+          <img src={footText3} className="footText 3"/>
+          <a href= "Github.com">FootText 3</a>
+          </div>
     </header>
   );
 }
