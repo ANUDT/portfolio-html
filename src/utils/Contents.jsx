db8b262d54f4ac0f545d8001bf94993dc651d6f8
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-07 14:09:05 +0100

    contents.jsx darktheme

---
+++ src/utils/Contents.jsx
@@ -0,0 +1,17 @@
+export const darkTheme = {
+      background: "#1C1C27",
+      backgroundLight: "#854CE6",
+      Primary: "#19212C",
+      text1: "#C8CFD8",
+      text2: "#F2F5F7",
+      text3: "#626970",
+      text4: "#575C66",
+      card:"#FFFFF",
+      card_Light:"#161616",
+      button: "#854CEF",
+      black: "000000",      
+      footerBackground: "#00012B"
+    },
+   
+    },
+  
