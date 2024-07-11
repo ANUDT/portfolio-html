843dec13b09c281607af2c485fa39adf6794d99b
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-11 19:14:23 +0100

    test connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 //single page application creation  for react and router client side
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styles, { ThemeProvider } from 'styled-components';
-import Header from './components/header';
-import Footer from './components/footer';
-import AboutMe from './components/aboutMe';
-import Contact from './components/contact';
-import Project from './components/project';
-import Resume from './components/resume';
-import Skills from './components/skills.jsx';
-import { lightTheme } from './utils/Themes.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { ThemeProvider } from 'styled-components';
+// import Header from './components/header';
+// import Footer from './components/footer';
+// import AboutMe from './components/aboutMe';
+// {/*import Contact from './components/contact';
+// import Project from './components/project';
+// import Resume from './components/resume';
+// import Skills from './components/skills.jsx';*/}
+// import { lightTheme } from './utils/Themes.jsx';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
 
   //displaying app with the elements header, footer, about me, contact, navigation, portfolio, project and resume
 // it will render following the path given
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
-        <Router>
-          <Header />
-              <Routes>
-              <Route path="/" element={<AboutMe />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/project" element={<Project />} />
-              <Route path="/resume" element={<Resume />} />
-              <Route path="/skills" element={<Skills />} />
-            </Routes>
-           <Footer />
-        </Router>
-      </ThemeProvider>  
+      <>Hallo</>
+    // <ThemeProvider theme={lightTheme}>
+    //     <Router>
+    //       <Header />
+    //           <Routes>
+    //           <Route path="/" element={<AboutMe />} />
+    //           {/* <Route path="/contact" element={<Contact />} />
+    //           <Route path="/project" element={<Project />} />
+    //           <Route path="/resume" element={<Resume />} />
+    //           <Route path="/skills" element={<Skills />} /> */}
+    //         </Routes>
+    //        <Footer />
+    //     </Router>
+    //   </ThemeProvider>  
   );
 }
 
