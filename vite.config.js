4454b39e7fed406e0265c2d9e76104c545e3f293
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-11 19:30:30 +0100

    changes commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
+console.log ("filename", __filename)
 
 const __dirname = path.dirname(__filename);
-
+console.log ("Dirname", __dirname)
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   root: path.resolve(__dirname, 'src'),
+    root: path.resolve(__dirname, './src'),
     server: {
     port: 3001,
-    open: true
+    hot: true
   },
 });
 
